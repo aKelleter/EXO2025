6837fdc946520f6a6fa95801b54323102c0cef46
Alain Kelleter <alain.kelleter@gmail.com>
2024-11-26 09:11:32 +0100

    Update 0.6.1
    - Mises à jour mineures

--- conf.php
+++ conf.php
@@ -1,7 +1,7 @@
 <?php
 // Définition des constantes de l'application
 const APP_NAME = 'EXO 2025';
-const APP_UPDATE = '25112024 - 1615';
-const APP_VERSION = 'v0.6.0'.'-'.APP_UPDATE;
+const APP_UPDATE = '26112024 - 1610';
+const APP_VERSION = 'v0.6.1'.'-'.APP_UPDATE;
 const APP_YEAR = '2024-2025';
 const APP_AUTHOR = 'A. Kelleter';
