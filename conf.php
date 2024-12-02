5e2259a04c83d9a8bfa373d6750ab5137276bc0d
Alain Kelleter <alain.kelleter@kelleter.be>
2024-12-02 15:57:04 +0100

    Update 0.10.1
    - Add favicon

--- conf.php
+++ conf.php
@@ -1,8 +1,8 @@
 <?php
 // Définition des constantes de l'application
 const APP_NAME = 'ExO 2025';
-const APP_UPDATE = '02122024 - 1620';
-const APP_VERSION = APP_NAME.' - LPB|LDB - v0.10.0'.'-'.APP_UPDATE;
+const APP_UPDATE = '02122024 - 1625';
+const APP_VERSION = APP_NAME.' - LPB|LDB - v0.10.1'.'-'.APP_UPDATE;
 const APP_YEAR = '2024-2025';
 const APP_AUTHOR = 'A. Kelleter';
 const DS = DIRECTORY_SEPARATOR;
